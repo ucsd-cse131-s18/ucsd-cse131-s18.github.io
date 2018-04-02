54582eda9a49ffebddda52aea778c451374917d1
Joe Gibbs Politz <joe.politz@gmail.com>
2018-04-02 13:00:35 -0700

    .

---
+++ GetLectureLink.py
@@ -0,0 +1,33 @@
+import os
+
+import glob
+
+path = 'ucsd-cse12-w17.github.io/lectures/'
+lecture_path='./lectures'
+url_protocol='https://'
+
+master = open('README.md','w')
+master.write('https://ucsd-cse131-s18.github.io\n\nLecture notes links:\n')
+os.chdir(lecture_path)
+
+
+lectures = glob.glob('./*')
+
+
+for lecture in sorted(lectures):
+
+  os.chdir(lecture)
+
+  current = lecture.split('/')[-1]
+  
+  files = glob.glob('./*')
+
+  for f in sorted(files):
+    name=f.split('.')
+    if name[-1] == 'html':
+      url = url_protocol+path+current+f[1:]
+      master.write(url+'\n\n')
+
+  os.chdir('..')
+
+master.close()
